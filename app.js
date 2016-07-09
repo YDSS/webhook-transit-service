546872cd884711262ddae9710211b973e3fc5a9e
YDSS <y.dsheng10@gmail.com>
2016-07-10 00:20:15 +0800

    almost finish, only one bug to fix

--- app.js
+++ app.js
@@ -3,17 +3,29 @@
  * @author YDSS
  */
 'use strict';
+const fs = require('fs');
+const path = require('path');
+const execFile = require('child_process').execFile;
+
 const koa = require('koa');
+const logger = require('koa-logger');
+const koaBody = require('koa-body');
+
 const app = koa();
+const env = app.env;
 const port = 3000;
 
-// filter request not from github domain
-const reFilter = /github/;
+app.env === 'development' && app.use(logger());
+app.use(koaBody());
+
+// filter req not from webhook
+const reFilter = /^\/webhook(\/[^\/])?/
 app.use(function* (next) {
-    if (reFilter.test(this.path) 
+    if (reFilter.test(this.path)
         // see part of Delivery headers in `https://developer.github.com/webhooks/` 
-        && this.headers['X-GitHub-Event']
-        && this.headers['User-Agent'].indexOf('GitHub-Hookshot')) {
+        // headers key always lower case
+        && this.headers['x-github-event']
+        && this.headers['user-agent'].indexOf('GitHub-Hookshot') > -1) {
         yield next;
     }
     else {
@@ -22,10 +34,60 @@ app.use(function* (next) {
     }
 });
 
+// exec shell to update repo
 app.use(function* (next) {
-    let body = this.request.body;
-    debugger
+    try {
+        // payload is a string
+        console.log(123);
+        let payload = JSON.parse(this.request.body.payload);
+        let pusher = payload.pusher;
+        let gitRepoName = payload.repository.name;
+        let owner = payload.repository.owner.name; 
+        
+        // only owner's pushes are allowed
+        if (owner !== pusher.name) {
+            this.status = 403;
+            this.body = `only ${owner} can trigger webhook`;
+            return;
+        }
+        
+        console.log('before exec');
+        console.log(this.status);
+        execFile(
+            path.join(__dirname, './bin/git_opt'), 
+            // no need space between option and value
+            [`-d${gitRepoName}`], 
+            (err, stdout, stderr) => {
+                console.log(this.status);
+                if (err) {
+                    this.status = 500;
+                    this.body = 'exec shell failed';
+
+                    console.log(err);
+                }
+                console.log(this.status);
+                console.log(this.body);
+
+                // this.body = 'update success'
+                console.log(stdout);
+            }
+        );
+    } catch (e) {
+        console.error(e);
+    }
 });
 
 app.listen(port);
 console.log(`server start at port: ${port}`);
+
+app.on('error', (err) => {
+    console.log('fatal error: ' + err);
+    // del pid file
+    fs.unlink('./pid');  
+});
+
+// write pid in current dir in case of killing this process
+const pid = process.pid;
+fs.writeFile('./pid', pid, () => {
+    console.log('pid store in file pid');
+});
